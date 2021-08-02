789c96b76daac0d088991c89de6bded522ae9395
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-01 21:40:33 -0400

    Using ant design for styling

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,10 +1,26 @@
 import React, {useState} from 'react'
+import { Typography, Button, Form, message, Input, Icon} from 'antd';
+
+const{Title} = Typography;
+const {TextArea} = Input;
+
+
+const Continents = [
+    {key:1, value: "Africa"},
+    {key:2, value: "Europe"},
+    {key:3, value: "Asia"},
+    {key:4, value: "North America"},
+    {key:5, value: "South America"},
+    {key:6, value: "Australia"},
+    {key:7, value: "Antarctica"}
+]
 
 function UploadProductPage() {
 
     const [TitleValue, setTitleValue] = useState("")
     const [DescriptionValue, setDescriptionValue] = useState("")
     const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
 
     const onTitleChange = (event) => {
         setTitleValue(event.currentTarget.value)
@@ -18,51 +34,55 @@ function UploadProductPage() {
         setPriceValue(event.currentTarget.value)
     }
 
+    const onContinentsSelectChange = (event) => {
+        setContinentValue(event.currentTarget.value)
+    }
+
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
             <div style={{ textAlign:'center', marginBottom:'2rem'}}>
-                <h2> Upload Product</h2>
+                <Title level={2}> Upload Product</Title>
             </div>
             
-            <form onSubmit>
+            <Form onSubmit>
 
             <br />
             <br />
             <label>Title</label>
-            <input
+            <Input
                 onChange={onTitleChange}
                 value={TitleValue}
             />
             <br />
             <br />
             <label>Description</label>
-            <textarea
+            <TextArea
                 onChange={onDescriptionChange}
                 value={DescriptionValue}
             />
             <br />
             <br />
             <label>Price</label>
-            <input 
+            <Input 
                 onChange={onPriceChange}
                 value={PriceValue}
                 type="number"
             />
-            <select>
-                <option key value>
-
-                </option>
+            <select onChange={onContinentsSelectChange}>
+                {Continents.map(item => (
+                    <option key={item.key} value={item.key}>{item.value}</option>
+                ))}
             </select>
             <br />
             <br />
 
-            <button
+            <Button
                 onClick
             >
                 Submit
-            </button>
+            </Button>
 
-            </form>
+            </Form>
             
 
 
