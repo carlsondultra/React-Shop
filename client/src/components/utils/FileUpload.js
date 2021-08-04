f0b25ae9ce0b60fd67d07dffa15649d7573bbfac
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-03 20:14:34 -0400

    Added deleting of pictures after uploading them

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -29,6 +29,18 @@ function FileUpload(props) {
             })
     }
 
+    //Deleting images after being uploaded 
+    const onDelete = (image) => {
+        const currentIndex = Images.indexOf(image);
+
+        let newImages = [...Images]
+        newImages.splice(currentIndex, 1)
+
+        setImages(newImages)
+        props.refreshFunction(newImages)
+
+    }
+
 
     return (
         <div style={{ display:'flex', justifyContent:'space-between'}}>
@@ -54,7 +66,7 @@ function FileUpload(props) {
             <div style={{ display:'flex', width:'350px', height:'240px', overflowX:'scroll'}}>
                     
                 {Images.map((image, index) => (
-                <div onClick>
+                <div onClick={() => onDelete(image)}>
                     <img style={{minWidth:'300px', width:'300px', height:'240px' }} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`}/>
                 </div>
                 ))}
