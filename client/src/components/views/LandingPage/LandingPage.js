8e6f296da7275b2572051d43a77fa11995631b00
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-04 19:44:40 -0400

    Products can now be displayed on landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import Axios from 'axios';
+import {Icon, Col, Card, Row} from 'antd';
+
+const {Meta} = Card;
 
 function LandingPage() {
 
@@ -10,7 +13,7 @@ function LandingPage() {
         .then(response => {
             if(response.data.success) {
                 setProducts(response.data.products)
-                
+
                 console.log(response.data.products)
             } else {
                 alert('Failed to fetch product data')
@@ -19,11 +22,50 @@ function LandingPage() {
 
     }, [])
     
+    const renderCards = Products.map((product, index) => {
+
+        return <Col lg={6} md={8} xs={24}>
+            <Card
+                hoverable={true}
+                cover
+                
+            >
+
+            <Meta 
+                title ={product.title}
+                description={`$${product.price}`}
+                />
+
+            </Card>
+        
+        </Col>
+
+    })
 
     return (
-        <>
+        <div style={{width: '75%', margin: '3rem auto'}}>
+            <div style={{textAlign: 'center'}}>
+                <h2>Welcome! <Icon type="rocket"/></h2>
+            </div>
 
-        </>
+            {Products.length === 0? 
+            <div style={{display:'flex', height: '300px', justifyContent: 'center', alignItems: 'center'}}>
+                <h2>Nothing posted</h2>
+            </div> :
+            <div>
+
+                <Row gutter={[16,16]}>
+                    {Products.map((product, index) => {})}
+                    {renderCards}
+                </Row>
+
+            </div>
+            }
+            <br></br>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
     )
 }
 
