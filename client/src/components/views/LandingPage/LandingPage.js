5e60c4be267b30579582a77578c7f81e9eeed596
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-04 21:52:01 -0400

    Adding load more functionality to landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,20 +8,47 @@ const {Meta} = Card;
 function LandingPage() {
 
     const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
 
     useEffect(() => {
-        Axios.post('/api/product/getProducts')
+
+        const variables = {
+            skip: Skip,
+            limit: Limit,
+            
+        }
+
+        getProducts(variables)
+    }, [])
+
+    const getProducts = (variables) => {
+        Axios.post('/api/product/getProducts', variables)
         .then(response => {
             if(response.data.success) {
-                setProducts(response.data.products)
+                setProducts(...Products, [response.data.products])
 
                 console.log(response.data.products)
             } else {
                 alert('Failed to fetch product data')
             }
         })
+    }
 
-    }, [])
+
+    const onLoadMore = () => {
+
+    let skip = Skip + Limit;
+
+    const variables = {
+        skip: skip,
+        limit: Limit,
+
+    }
+
+    getProducts(variables)
+
+    }
     
     const renderCards = Products.map((product, index) => {
 
@@ -64,7 +91,7 @@ function LandingPage() {
             }
             <br></br>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <button>Load More</button>
+                <button onClick={onLoadMore}>Load More</button>
             </div>
         </div>
     )
