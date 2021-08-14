f16b385519ab6f03e6f748057259c1266cc56985
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-14 15:02:24 -0400

    Beginning product page, able to click on products

---
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -0,0 +1,18 @@
+import Axios from 'axios'
+import React, {useEffect} from 'react'
+
+function DetailProductPage() {
+
+    useEffect(() => {
+       // Axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
+        
+    }, [])
+
+    return (
+        <div>
+            DetailProductPage
+        </div>
+    )
+}
+
+export default DetailProductPage
