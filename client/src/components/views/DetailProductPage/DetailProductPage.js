bdf6340cdb9e15a507824048e5becc460cb6c05c
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-14 15:52:49 -0400

    Images visible on Product Page

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,5 +1,8 @@
 import Axios from 'axios'
 import React, {useEffect, useState} from 'react'
+import {Row, Col} from 'antd'
+import ProductImage from './Sections/ProductImage'
+import ProductInfo from './Sections/ProductInfo'
 
 function DetailProductPage(props) {
 
@@ -14,8 +17,21 @@ function DetailProductPage(props) {
     }, [])
 
     return (
-        <div>
-            DetailProductPage
+        <div className="postPage" style={{width: '100%', padding: '3rem 4rem'}}>
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <h1>{Product.title}</h1>
+            </div>
+            <br />
+
+            <Row gutter={[16, 16]} >
+                <Col lg={12} xs={24}>
+                    <ProductImage detail={Product} />
+                </Col>
+                <Col lg={12} xs={24}>
+                    <ProductInfo detail={Product}/>
+                </Col>
+            </Row>
         </div>
     )
 }
