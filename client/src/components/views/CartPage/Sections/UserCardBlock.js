dcf11a9433676096c16e7f86e1413f6a58832d7b
carlsondultra <carlsondultra@users.noreply.github.com>
2021-08-19 22:56:31 -0400

    Creating Cart Page, adding table to Cart Page

---
+++ client/src/components/views/CartPage/Sections/UserCardBlock.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function UserCardBlock() {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product Image</th>
+                        <th>Product Quantity</th>
+                        <th>Product Price</th>
+                        <th>Remove from Cart</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
+
+export default UserCardBlock
